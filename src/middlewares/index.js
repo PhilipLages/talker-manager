dd6ae89efc54ad8f2e5103eaf375cc7fca2e16d4
Philip Lages <psilvaelages@gmail.com>
2022-11-04 21:17:03 -0300

    feat (requirement 5): add token and name validation

---
+++ src/middlewares/index.js
@@ -0,0 +1,53 @@
+const validateEmail = (req, res, next) => {
+    const { email } = req.body;
+    const emailRegex = /^\S+@\S+\.\S+$/;
+
+    const isEmailValid = emailRegex.test(email);
+    
+    if (!email) {
+        return res.status(400).send({ message: 'O campo "email" é obrigatório' });
+    } if (!isEmailValid) {
+        return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
+    } 
+    next();
+};
+
+const validatePassword = (req, res, next) => {
+    const { password } = req.body;
+
+    if (!password) {
+        return res.status(400).send({ message: 'O campo "password" é obrigatório' });
+    } if (password.length < 6) {
+        return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
+    } 
+    next();
+};
+
+const validateToken = (req, res, next) => {
+    const token = req.headers.authorization;
+
+    if (!token) {
+        return res.status(401).send({ message: 'Token não encontrado' });
+    } if (token.length !== 16 || typeof token !== 'string') {
+        return res.status(401).send({ message: 'Token inválido' });
+    }
+    next();
+};
+
+const validateName = (req, res, next) => {
+    const { name } = req.body;
+
+    if (!name) {
+        return res.status(400).send({ message: 'O campo "name" é obrigatório' });
+    } if (name.length < 3) {
+        return res.status(400).send({ message: 'O "name" deve ter pelo menos 3 caracteres' });
+    } 
+    next();
+};
+
+module.exports = {
+    validateEmail,
+    validatePassword,
+    validateToken,
+    validateName,
+};
